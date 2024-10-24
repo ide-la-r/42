dcecdd9e61380be0f571b9125547cf871b176dee
ide-la-r <ism17v2@gmail.com>
2024-10-24 17:01:23 +0200

    corregido varias cosas de funciones 42

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,13 +6,11 @@
 /*   By: ide-la-r <ide-la-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 17:01:07 by ide-la-r          #+#    #+#             */
-/*   Updated: 2024/10/21 18:14:17 by ide-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/24 16:14:28 by ide-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-#include <stdio.h>
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ide-la-r <ide-la-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:20:10 by ide-la-r          #+#    #+#             */
-/*   Updated: 2024/10/21 18:16:25 by ide-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/24 16:26:08 by ide-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,22 @@ void	*ft_memmove(void *dest, const void *str, size_t n)
 {
 	unsigned char		*d;
 	unsigned const char	*s;
-	size_t	i;
+	size_t				i;
 
-	d = (unsigned char*)dest;
-	s = (unsigned char*)str;
+	d = (unsigned char *)dest;
+	s = (unsigned char *)str;
 	i = 0;
 	if (!s && !d)
 		return (0);
 	if (s < d)
-        ft_memcpy(dest, str, n);
+		ft_memcpy(dest, str, n);
 	else
-        while (n-- > 0)
-            *(d++) = *(s++);
+	{
+		while (n-- > 0)
+		{
+			*(d++) = *(s++);
+		}
+	}
 	return (dest);
 }
 
