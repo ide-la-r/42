a0f85319743f12909f9b919f6866f7b2beee98e7
ide-la-r <ism17v2@gmail.com>
2024-10-29 18:21:51 +0100

    Añadidos funciones strlcat, tolower y toupper

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -12,13 +12,25 @@
 
 #include "libft.h"
 
-ft_strlcat(char *dst, const char *src, size_t size)
+size_t  ft_strlcat(char *dest, const char *src, size_t n)
 {
-    size_t  i;
-    size_t  j;
+    size_t  dest_len;
+    size_t  src_len;
 
-    i = ft_strlen(dst);
-    j = ft_strlen(src);
-    
-    return (src);
+    dest_len = ft_strlen(dest);
+    src_len = ft_strlen(src);
+    if (dest_len >= n)
+        return (src_len + n);
+    size_t  space_left;
+    size_t  i;
+
+    space_left = n - dest_len -1;
+    i = 0;
+    while (i < space_left && src[i] != '\0')
+    {
+        dest[dest_len + i] = src[i];
+        i++;
+    }
+    dest[dest_len + i] = '\0';
+    return (dest_len + src_len);
 }
--- Libft/libft.h
+++ Libft/libft.h
@@ -25,5 +25,8 @@ void			ft_bzero(void *str, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
 void	        *ft_memmove(void *dest, const void *str, size_t n);
 size_t          ft_strlcpy(char *dest, const char *src, size_t n);
+size_t          ft_strlcat(char *dest, const char *src, size_t n);
+int             ft_toupper(int c);
+int             ft_tolower(int c);
 
 #endif
