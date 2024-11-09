d07a3254b2c97a9a09a3241b531ca78b97f5bab6
ide-la-r <ism17v2@gmail.com>
2024-11-09 18:10:59 +0100

    Hecho la funcion ft_atoi y añadida a el libft.h

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -10,7 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+static int  ft_isspace(char c)
+{
+    return (c == ' ' || c == '\f' || c == '\n' || c == '\r'
+        || c == '\t' || c == '\v');
+}
+
 int ft_atoi(const char *str)
 {
+    size_t  i;
+    int sign;
+    int res;
 
+    i = 0;
+    res = 0;
+    sign = 1;
+    while (ft_isspace(str[i]))
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        res = res * 10 + (str[i] - '0');
+        i++;
+    }
+    return (res * sign);
 }
